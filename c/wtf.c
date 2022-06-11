194d15dc63f8d9ec6b694d9b6959985c50f3ead7
insanolanbiri <erenakgun2007@hotmail.com>
2022-06-11 13:41:48 +0300

    tam sorunun eklenmesi

--- c/wtf.c
+++ c/wtf.c
@@ -6,11 +6,13 @@
 #include <stdio.h>
 
 /**
- * @brief bilim olimpiyatları sorusu kendisi. nasıl olduğunu çözebilmiş değilim. 
+ * @brief bilim olimpiyatları sorusu kendisi (2016 Bilgisayar A kitapçığı soru 42). nasıl olduğunu çözebilmiş değilim.
  */
-int main(int argc, char const *argv[])
-{
-    int i = 0;
-    printf("wtf: %d,%d,%d,%d\n", i++, i++, ++i, ++i); // ???
+int main(){
+    int d[][3][2]={4,5,6,7,8,9,10,11,12,13,14,15,16};
+    int i=-1;
+    int j;
+    j=d[i++][++i][++i]; // neden 4 (yani d[0][0][0])
+    printf("%d",j);
     return 0;
 }
